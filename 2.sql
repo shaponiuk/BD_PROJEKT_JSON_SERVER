1696cdb41b2327fe6131a801202987f54639dc29
Szymon Haponiuk <haponiukszymon@gmail.com>
2019-02-03 17:49:05 +0100

    show all zasoby

--- 2.sql
+++ 2.sql
@@ -1,4 +1,5 @@
 INSERT INTO zasoby VALUES (0, 'rekawiczki', 300);
-INSERT INTO zasoby VALUES (0, 'lozko', 2)
+INSERT INTO zasoby VALUES (1, 'lozko', 2);
+INSERT INTO zasoby VALUES (2, 'bandaz', 350);
 
 COMMIT;
