d0aac3d501be78fce7e827bc782278cd1160b98a
Szymon Haponiuk <haponiukszymon@gmail.com>
2019-02-05 17:55:55 +0100

    sql files

--- 2.sql
+++ 2.sql
@@ -6,11 +6,11 @@ INSERT INTO statusy_uslug VALUES (0, 'umowiona');
 INSERT INTO statusy_uslug VALUES (1, 'zakonczona');
 
 INSERT INTO specjalizacje VALUES (0, 'chirurgia');
-INSERT INTO specjalizacje VALUES (1, 'neurologia')
+INSERT INTO specjalizacje VALUES (1, 'neurologia');
 
 INSERT INTO lekarze VALUES (0, 'Bogdan', 'Lekarski', 0);
 INSERT INTO lekarze VALUES (1, 'Abraham', 'Lekarz', 1);
 
-INSERT INTO pacjenci (11111111111, 'Eugeniusz', 'Caweczynski');
+INSERT INTO pacjenci VALUES (11111111111, 'Eugeniusz', 'Caweczynski');
 
 COMMIT;
