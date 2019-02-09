f82685a8f639214d3f5d7fbaf975d525cf57fbd0
Szymon Haponiuk <haponiukszymon@gmail.com>
2019-02-09 17:00:47 +0100

    show doctors

--- src/Queries/ShowDoctors.kt
+++ src/Queries/ShowDoctors.kt
@@ -1,5 +1,6 @@
 package Queries
 
+import Queries.Constants.Companion.ERROR_STRING
 import Queries.SQLConnection.Companion.con
 import java.sql.ResultSet
 
@@ -13,17 +14,43 @@ class ShowDoctors {
     val lambda: (Map<String, List<String?>>) -> String =
       {
         if (it.containsKey(withSpecialisationKey)) {
-          if (it.containsKey(byIdKey)) {
-            TODO()
+          if (checkArgument(it, withSpecialisationKey)) {
+            if (it.containsKey(byIdKey)) {
+              if (checkArgument(it, byIdKey)) {
+                showDoctorsWithSpecialisationById(it)
+              } else {
+                ERROR_STRING
+              }
+            } else {
+              showDoctorsWithSpecialisation(it)
+            }
           } else {
-  TODO()
+            ERROR_STRING
           }
         } else if (it.containsKey(byIdKey)) {
-  TODO()
+          if (checkArgument(it, byIdKey)) {
+            showDoctorsById(it)
+          } else {
+            ERROR_STRING
+          }
         } else {
-          showAll()
+          showAllDoctors()
         }
       }
+
+    private fun checkArgument(map: Map<String, List<String?>>, key: String): Boolean {
+      val argList = map[key]
+
+      return if (argList != null) {
+        if (!argList.isEmpty()) {
+          return argList[0] != null
+        } else {
+          false
+        }
+      } else {
+        false
+      }
+    }
 
     private fun printRs(rs: ResultSet): String {
       var rStr = "["
@@ -47,11 +74,60 @@ class ShowDoctors {
       return rStr
     }
 
-    private fun showAll(): String {
+    private fun showDoctorsWithSpecialisationById(map: Map<String, List<String?>>): String {
+      val specialisationId = map[withSpecialisationKey]!![0]!!
+      val doctorId = map[byIdKey]!![0]!!
+
       val stmt = con.createStatement()
-      val rs = stmt.executeQuery("""
+      val rs = stmt.executeQuery(
+        """
+          SELECT *
+          FROM lekarze
+          WHERE specjalizacje_id = $specialisationId
+          AND id = $doctorId
+        """.trimIndent()
+      )
+
+      return printRs(rs)
+    }
+
+    private fun showDoctorsWithSpecialisation(map: Map<String, List<String?>>): String {
+      val specialisationId = map[withSpecialisationKey]!![0]!!
+
+      val stmt = con.createStatement()
+      val rs = stmt.executeQuery(
+        """
+          SELECT *
+          FROM lekarze
+          WHERE specjalizacje_id = $specialisationId
+        """.trimIndent()
+      )
+
+      return printRs(rs)
+    }
+
+    private fun showDoctorsById(map: Map<String, List<String?>>): String {
+      val doctorId = map[byIdKey]!![0]!!
+
+      val stmt = con.createStatement()
+      val rs = stmt.executeQuery(
+        """
+          SELECT *
+          FROM lekarze
+          WHERE id = $doctorId
+        """.trimIndent()
+      )
+
+      return printRs(rs)
+    }
+
+    private fun showAllDoctors(): String {
+      val stmt = con.createStatement()
+      val rs = stmt.executeQuery(
+        """
         SELECT * FROM lekarze
-      """.trimIndent())
+      """.trimIndent()
+      )
 
       return printRs(rs)
     }
